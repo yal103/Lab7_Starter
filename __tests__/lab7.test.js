a03630acc315d6c89836968b298db8ac034fedcc
yal103 <yal103@ucsd.edu>
2025-05-18 18:35:34 -0700

    expose: step 1

--- __tests__/lab7.test.js
+++ __tests__/lab7.test.js
@@ -21,7 +21,7 @@ describe('Basic user flow for Website', () => {
   // Check to make sure that all 20 <product-item> elements have data in them
   // We use .skip() here because this test has a TODO that has not been completed yet.
   // Make sure to remove the .skip after you finish the TODO. 
-  it.skip('Make sure <product-item> elements are populated', async () => {
+  it('Make sure <product-item> elements are populated', async () => {
     console.log('Checking to make sure <product-item> elements are populated...');
 
     // Start as true, if any don't have data, swap to false
@@ -52,7 +52,15 @@ describe('Basic user flow for Website', () => {
       it checks every <product-item> it found
     * Remove the .skip from this it once you are finished writing this test.
     */
+    for (let i = 1; i < prodItemsData.length; i++) {
+        console.log(`Checking product item ${i + 1}/${prodItemsData.length}`);
+        const firstVal = prodItemsData[i];
+        if (firstVal.title.length == 0) { allArePopulated = false; }
+        if (firstVal.price.length == 0) { allArePopulated = false; }
+        if (firstVal.image.length == 0) { allArePopulated = false; }
+    }
 
+    expect(allArePopulated).toBe(true);
   }, 10000);
 
   // Check to make sure that when you click "Add to Cart" on the first <product-item> that
